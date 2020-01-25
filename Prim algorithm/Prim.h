80392df355e43f33235723fa9efd7dc5d262e55f
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-01-25 20:10:03 +0200

    Create Prim.h

---
+++ Prim algorithm/Prim.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "EdgeList.h"
+#include <string>
+
+const size_t gr = 10;
+
+class Prim
+{
+	EdgeList *edges = new EdgeList();
+
+public:
+	std::string getMST(const int someGraph[gr][gr], int vertex);
+
+private:
+	int graphCopy[gr][gr];
+	int mst[gr][gr];
+	void copyGraph(const int someGraph[gr][gr]);
+	bool visited[gr];
+	bool visitedLoop[gr];
+	int parentLoop[gr];
+	std::string printResult();
+};
