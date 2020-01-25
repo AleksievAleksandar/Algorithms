a0a062cdf86f6ce27819c9dacfd94e2b77ea83bb
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-01-25 20:18:13 +0200

    Create Kruskal.h

---
+++ Kruskal algorithm/Kruskal.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Neighbours.h"
+#include <string>
+
+class Kruskal
+{
+	Neighbours* neighbours = new Neighbours();
+	Neighbours* MST = new Neighbours();
+
+public:
+	std::string getResult(const int someGraph[GraphSize][GraphSize]);
+
+private:
+	int roots[GraphSize];
+	void setRoots();
+	void updateRoots();
+	int rootForUpdate;
+	int rootForUpdSec;
+	void setMST();
+	std::string printMST();
+};
