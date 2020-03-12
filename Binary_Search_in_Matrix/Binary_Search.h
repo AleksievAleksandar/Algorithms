532a02839d83855a7c39f8be4cb142e7b10cada3
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-03-12 14:39:17 +0200

    Create Binary_Search.h

---
+++ Binary_Search_in_Matrix/Binary_Search.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <sstream>
+const int martixSize = 12;
+
+class BinarySearch
+{
+public:
+	size_t cnt = 1;
+public:
+	void findTarget(int martix[martixSize][martixSize], int &y, int &x, int yTarget, int xTarget);
+	std::string printMatrix(int matrix[martixSize][martixSize]);
+};
