295e2ef81864dcec9fe852bfd7ff621961315a7b
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-03-12 14:38:39 +0200

    Create Binary_Search.cpp

---
+++ Binary_Search_in_Matrix/Binary_Search.cpp
@@ -0,0 +1,60 @@
+#include "Binary_Search.h"
+
+void BinarySearch::findTarget(int martix[martixSize][martixSize], int &y, int &x, int yTarget, int xTarget)
+{
+	martix[y][x] = -1;
+	martix[yTarget][xTarget] = 1;
+	while (y != yTarget || x != xTarget)
+	{
+		if (y < yTarget && x < xTarget)
+		{
+			y = (martixSize + y) / 2;
+			x = (martixSize + x) / 2;
+			martix[y][x] = cnt;
+		}
+		else if (y > yTarget && x < xTarget)
+		{
+			y /= 2;
+			x = (martixSize + x) / 2;
+			martix[y][x] = cnt;
+		}
+		else if (y > yTarget && x == xTarget)
+		{
+			y /= 2;
+			martix[y][x] = cnt;
+		}
+		else if (y < yTarget && x == xTarget)
+		{
+			y = (martixSize - y) / 2;
+			martix[y][x] = cnt;
+		}
+		else if (y < yTarget && x > xTarget)
+		{
+			y = (martixSize + y) / 2;
+			x /= 2;
+			martix[y][x] = cnt;
+		}
+		else if (y == yTarget && x > xTarget)
+		{
+			x /= 2;
+			martix[y][x] = cnt;
+		}
+		cnt++;
+	}
+}
+
+std::string BinarySearch::printMatrix(int matrix[martixSize][martixSize])
+{
+	std::ostringstream info;
+
+	for (size_t i = 0; i < martixSize; i++)
+	{
+		for (size_t a = 0; a < martixSize; a++)
+		{
+			info << matrix[i][a] << " ";
+		}
+		info << "\n";
+	}
+
+	return info.str();
+}
