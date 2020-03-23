b6998f858901ff5c0a20692b5b3240f926cbaa58
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-03-23 20:03:36 +0200

    Update Main.cpp

--- Journey Task/Main.cpp
+++ Journey Task/Main.cpp
@@ -3,52 +3,92 @@
 #include "FlightsGenerator.h"
 #include <vector>
 
-int arr[5] = { 1, 2, 3, 4, 5 };
+const int arrSize = 6;
+const int howManyTickets = 4;
 
-bool visited[5][5] = {};
+int arr[arrSize] = { 1, 2, 3, 4, 5, 6};
+int result[arrSize][arrSize];
+
+int howMany = arrSize - 2;
+
+bool lastIsVisit = false;
+
+void clearCurrent(int level)
+{
+	for (size_t i = 0; i < arrSize; i++)
+	{
+		result[level][i] = 0;
+	}
+}
 
 void printArray(int someArr[], int start, int end)
 {
+	for (size_t i = arrSize - 1; i > 0; i--)
+	{
+		bool temp = false;
+		for (size_t a = 0; a < arrSize; a++)
+		{
+			if (result[i][a] != 0)
+			{
+				temp = true;
+				std::cout << result[i][a] << " ";
+			}
+		}
+		if (temp)
+		{
+			std::cout << "\n";
+		}
+	}
 	for (size_t i = start; i < end; i++)
 	{
 		std::cout << someArr[i] << " ";
 	}
 	std::cout << "\n";
+	std::cout << "-------------\n";
 }
 
-void gen(int someArr[5], int startPos, int endPos, int increase, int level)
+void gen(int someArr[6], int startPos, int endPos, int increase, int level)
 {
-	if (startPos >= endPos || visited[level][increase] == true || startPos < 0 || level > 3 || (startPos >= 3 && level == 3))
-	{
-		return;
-	}
-
-	if (level == 1 || startPos == endPos - 1)
+	if (level == 1 && lastIsVisit == false)
 	{
 		printArray(someArr, startPos, endPos);
+		lastIsVisit = true;
+		return;
+	}
+	if (startPos >= endPos)
+	{
+		return;
+	}
+	if (level == 1 && lastIsVisit == true)
+	{
 		return;
 	}
 
-	int getNext = startPos;
-	size_t cnt = 0;
-	while (cnt < increase)
+	lastIsVisit = false;
+	int newCnt = 0;
+	if (startPos > -1)
 	{
-		visited[level][increase] = true; // visited[getNext][increase] = true;
-		std::cout << arr[getNext] << " ";
-		getNext++;
-		cnt++;
-	}
-	std::cout << "\n";
-	startPos = getNext - 1;
-	for (size_t inc = 1; inc <= 3; inc++)
-	{	
-		gen(someArr, startPos + 1, endPos, inc, level - 1);
-		if (level < 3)
+		int getNext = startPos;
+		size_t cnt = 0;
+		clearCurrent(level);
+		while (cnt < increase)
 		{
-			level++;
-		}	
-		startPos--;	
-		visited[level][inc] = false;
+			result[level][getNext] = arr[getNext];
+			getNext++;
+			cnt++;
+			newCnt++;
+		}
+		startPos = getNext - 1;
+	}
+	int limit = howMany;
+	//if (level == 3)
+	//{
+		limit = howMany - newCnt + 1;
+	//}
+
+	for (size_t inc = 1; inc <= limit; inc++)
+	{
+		gen(someArr, startPos + 1, endPos, inc, level - 1);
 	}
 
 }
@@ -57,11 +97,9 @@ void gen(int someArr[5], int startPos, int endPos, int increase, int level)
 int main()
 {
 	int numberOfFlights = 0;
+	
 
-	//for (size_t i = 1; i <= 3; i++)
-	//{
-		gen(arr, 0, 5, 1, 3);
-	//}
+	gen(arr, -1, 6, howMany, howManyTickets);
 
 	return 0;
 }
