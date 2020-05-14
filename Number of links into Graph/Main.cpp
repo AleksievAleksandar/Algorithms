17c89a50989726c675c466f1b2225e9b14fbda57
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-05-14 18:28:22 +0300

    Create Main.cpp

---
+++ Number of links into Graph/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include "Links.h"
+
+const std::vector<std::vector<int> > graph =  { { 0, 0, 0, 0, 0, 0, 0 },
+												{ 0, 0, 1, 1, 0, 0, 0 }, 
+												{ 0, 1, 0, 1, 0, 0, 0 }, 
+												{ 0, 1, 1, 0, 0, 0, 0 }, 
+												{ 0, 0, 0, 0, 0, 1, 1 }, 
+												{ 0, 0, 0, 0, 1, 0, 1 }, 
+												{ 0, 0, 0, 0, 1, 1, 0 }, };
+
+int main()
+{
+	LinksOfConnections* conn = new LinksOfConnections();
+	std::cout << conn->findLinks(graph,1);
+	
+	getchar();
+	return 0;
+}
