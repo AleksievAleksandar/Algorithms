4656f57b19c22664a6c3ee42c16fecf6b384cf9a
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-05-25 14:54:20 +0300

    Update Words.h

--- SoftUni/Algorithms Exam - 21 Sept 2019/02. Word Cruncher/Words.h
+++ SoftUni/Algorithms Exam - 21 Sept 2019/02. Word Cruncher/Words.h
@@ -7,19 +7,20 @@ class Words
 {
 public:
 	Words() = default;
-	void generateWordsCombinations(std::string stringWords, std::string stringTarget);
+	std::string generateWordsCombinations(std::string stringWords, std::string stringTarget);
 
 private:
+	std::ostringstream solutions;
 	std::vector<std::string> words;
-	std::string target;
-	std::vector<std::string> combinations;
-	std::vector<bool> visited;
-	std::string finded;
+	std::string targetString;
+	std::vector<std::string> generatedCombinations;
+	std::vector<bool> visited; // this option must be implemented if we want not to use one and the same characters
 
 private:
 	void getAllWords(std::string stringWords);
 	void getTargetString(std::string stringWord);
-	void gen(std::vector<std::string> words, int index);
+	void generate(std::vector<std::string> words, int index, int startInddex);
 	void setFields();
 	void print();
+	bool findWord(std::string currentWord, int startIndex);
 };
