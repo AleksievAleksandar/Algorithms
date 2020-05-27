b1d1fce7405ac7290e048df3f2bee1a4c9e09479
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-05-27 15:28:46 +0300

    Update WordDifferences.h

--- SoftUni/Algorithms Exam - 21 Sept 2019/03. Word Differences/WordDifferences.h
+++ SoftUni/Algorithms Exam - 21 Sept 2019/03. Word Differences/WordDifferences.h
@@ -7,17 +7,15 @@ class WordDifferences
 {
 public:
 	WordDifferences() = default;
-	void findMinChanges(const std::string &first, const std::string &second);
+	int findMinChanges(const std::string &first, const std::string &second);
 
 private:
-	std::vector<char> charVector;
-	std::vector<char> charVectorTarget;
-	std::string firstWordCopy;
-	int minChanges = 10000;
-	int currentChanges = 0;
+	std::string stringToModify;
+	std::string stringTarget;
+
 private:
-	void getCharsFromString(const std::string &strOne, const std::string& strTwo);
-	void generateDiffs(std::vector<char> str, int index);
-	void deletion(int index);
-	void insertion(int index, char symbol);
+	int generateDiffs(const std::string &stringToModify, const std::string &stringTarget);
+	int min(const int &a, const int &b);
+	void setRowsAndCols(std::vector<std::vector<int>> &matrix);
+	int calculateDistance(std::vector<std::vector<int>>& matrix);
 };
