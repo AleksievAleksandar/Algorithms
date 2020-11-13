348b5cad6c6e6e8d2ee2a5626d5418c2c97cae44
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-11-13 17:11:35 +0200

    Update Main.cpp

--- Number of links into Graph/Main.cpp
+++ Number of links into Graph/Main.cpp
@@ -3,18 +3,18 @@
 #include "Links.h"
 
 const std::vector<std::vector<int> > graph =  { { 0, 0, 0, 0, 0, 0, 0 },
-												{ 0, 0, 1, 1, 0, 0, 0 }, 
-												{ 0, 1, 0, 1, 0, 0, 0 }, 
-												{ 0, 1, 1, 0, 0, 0, 0 }, 
-												{ 0, 0, 0, 0, 0, 1, 1 }, 
-												{ 0, 0, 0, 0, 1, 0, 1 }, 
-												{ 0, 0, 0, 0, 1, 1, 0 }, };
+						{ 0, 0, 1, 1, 0, 0, 0 }, 
+						{ 0, 1, 0, 1, 0, 0, 0 }, 
+						{ 0, 1, 1, 0, 0, 0, 0 }, 
+						{ 0, 0, 0, 0, 0, 1, 1 }, 
+						{ 0, 0, 0, 0, 1, 0, 1 }, 
+						{ 0, 0, 0, 0, 1, 1, 0 }, };
 
 int main()
 {
 	LinksOfConnections* conn = new LinksOfConnections();
 	std::cout << conn->findLinks(graph,1);
 	
-	getchar();
+	delete conn;
 	return 0;
 }
